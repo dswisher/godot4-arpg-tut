9fe9f2479071d5a604232a903dbf797ef473711b
Doug Swisher <big.swish@gmail.com>
2023-10-01 19:54:06 -0500

    Episode 3 complete

---
+++ README.md
@@ -0,0 +1,14 @@
+# Godot 4 ARPG Tutorial
+
+# Links
+
+* Videos
+    * [ARPG Playlist](https://www.youtube.com/playlist?list=PLMQtM2GgbPEVuTgD4Ln17ombTg6EahSLr)
+    * [JetBrains Rider IDE For Godot Game Development](https://www.youtube.com/watch?v=iSxhWWlHepM)
+* Godot Docs
+    * [Physics Introduction](https://docs.godotengine.org/en/latest/tutorials/physics/physics_introduction.html)
+    * [Idle and Physics Processing](https://docs.godotengine.org/en/latest/tutorials/scripting/idle_and_physics_processing.html#doc-idle-and-physics-processing)
+    * [AnimatedSprite2D](https://docs.godotengine.org/en/latest/classes/class_animatedsprite2d.html)
+* Assets
+    * [Ninja Asset Pack](https://pixel-boy.itch.io/ninja-adventure-asset-pack)
+
--- player/player.cs
+++ player/player.cs
@@ -2,12 +2,50 @@ using Godot;
 
 public partial class player : CharacterBody2D
 {
-    public const float Speed = 100.0f;
+    // TODO - how to make this visible in the inspector?
+    private const float Speed = 50.0f;
+
+    private AnimationPlayer animations;
+
+
+    public override void _Ready()
+    {
+        animations = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
 
     public override void _PhysicsProcess(double delta)
     {
         HandleInput();
         MoveAndSlide();
+        UpdateAnimation();
+    }
+
+
+    private void UpdateAnimation()
+    {
+        if (Velocity.Length() == 0)
+        {
+            animations.Stop();
+        }
+        else
+        {
+            var direction = "Down";
+            if (Velocity.X < 0)
+            {
+                direction = "Left";
+            }
+            else if (Velocity.X > 0)
+            {
+                direction = "Right";
+            }
+            else if (Velocity.Y < 0)
+            {
+                direction = "Up";
+            }
+
+            animations.Play("walk" + direction);
+        }
     }
 
 
